cc0d25ba45add49096aebf97098fbec067821daf
Jaime Lopez <detec.azul@gmail.com>
2018-03-31 14:34:20 +0100

    Fixed issue where spellslot level was using id key instead of level

--- models/spell_slot.rb
+++ models/spell_slot.rb
@@ -4,7 +4,7 @@ class SpellSlot
   def initialize(options)
     @id = options['id'].to_i unless options['id'] == nil
     @character_id = options['character_id'].to_i
-    @level = options['id'].to_i
+    @level = options['level'].to_i
     @quantity = options['quantity'].to_i
   end
 
