036b791edb0e3f7c471d54adc2e2e619c2c2c94c
Jaime Lopez <detec.azul@gmail.com>
2018-03-31 12:31:47 +0100

    Added a class for storing the information on c_classes and magic schools

---
+++ models/fixed_choices.rb
@@ -0,0 +1,27 @@
+class FixedChoices
+  self.classes()
+    return classes_array = [
+      "Bard",
+      "Cleric",
+      "Druid",
+      "Paladin",
+      "Ranger",
+      "Sorcerer",
+      "Warlock",
+      "Wizard"
+    ]
+  end
+
+  self.schools()
+    school_array = [
+      "Abjuration",
+      "Conjuration",
+      "Divination",
+      "Enchantment",
+      "Evocation",
+      "Illusion",
+      "Necromancy",
+      "Transmutation"
+    ]
+  end
+end
