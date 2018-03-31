26fe684063a2e01bdd4d6870825d0434789569e1
Jaime Lopez <detec.azul@gmail.com>
2018-03-31 13:24:01 +0100

    Basic CRUD actions on Spellbook class. Added spellbooks to the seed file

--- db/seed.rb
+++ db/seed.rb
@@ -1,6 +1,8 @@
 require_relative('../models/spell')
+require_relative('../models/spellbook')
 
 Spell.delete_all()
+Spellbook.delete_all()
 
 spell01_hash = {
   'name' => 'Acid Splash',
@@ -19,14 +21,14 @@ spell02_hash = {
 spell03_hash = {
   'name' => 'Fire Bolt',
   'school' => 'Evocation',
-  'level' => 0,
+  'level' => 1,
   'pages' => 1,
   'description' => "You hurl a mote of fire at a creature or object within range. Make a ranged spell attack against the target. On a hit, the target takes 1d10 fire damage. A flammable object hit by this spell ignites if it isn't being worn or carried. This spell's damage increases by 1d10 when you reach 5th level (2d10), 11th level (3d10), and 17th level (4d10)."
 }
 spell04_hash = {
   'name' => 'Magic Stone',
   'school' => 'Transmutation',
-  'level' => 0,
+  'level' => 1,
   'pages' => 1,
   'description' => "You touch one to three pebbles and imbue them with magic. You or someone else can make a ranged spell attack with one of the pebbles by throwing it or hurling it with a sling. If thrown, it has a range of 60 feet. If someone else attacks with the pebble, that attacker adds your spellcasting ability modifier, not the attacker’s, to the attack roll. On a hit, the target takes bludgeoning damage equal to 1d6 + your spellcasting ability modifier. Hit or miss, the spell then ends on the stone. If you cast this spell again, the spell ends early on any pebbles still affected by it."
 }
@@ -35,3 +37,15 @@ spell01 = Spell.new(spell01_hash).save()
 spell02 = Spell.new(spell02_hash).save()
 spell03 = Spell.new(spell03_hash).save()
 spell04 = Spell.new(spell04_hash).save()
+
+spellbook01_hash = {
+  "name" => "Level 1 Spellbook",
+  "pages" => 100
+}
+spellbook02_hash = {
+  "name" => "Level 2 Spellbook",
+  "pages" => 50
+}
+
+spellbook01 = Spellbook.new(spellbook01_hash).save()
+spellbook02 = Spellbook.new(spellbook02_hash).save()
--- models/spellbook.rb
+++ models/spellbook.rb
@@ -5,6 +5,36 @@ class Spellbook
     @id = options['id'].to_i unless options['id'] == nil
     @name = options['name']
     @pages = options['pages'].to_i
-    @character_id = options['character_id'].to_i
+    @character_id = options['character_id'].to_i unless options['character_id'] == nil
+  end
+
+  def self.all()
+    sql = "SELECT * FROM spellbooks"
+    spellbooks = SqlRunner.run(sql)
+    return spellbooks.map { |spellbook| Spellbook.new(spellbook) }
+  end
+
+  def self.delete_all()
+    sql = "DELETE FROM spellbooks"
+    SqlRunner.run(sql)
+  end
+
+  def self.find(id)
+    sql = "SELECT * FROM spellbooks
+    WHERE id = $1"
+    values = [id]
+    spellbook = SqlRunner.run(sql, values)
+    return Spellbook.new(spellbook.first) unless spellbook.first == nil
+  end
+
+  def save()
+    sql = "INSERT INTO spellbooks
+    ( name, pages, character_id )
+    VALUES
+    ( $1, $2, $3 )
+    RETURNING id"
+    values = [@name, @pages, @character_id]
+    result = SqlRunner.run(sql, values)
+    @id = result.first['id'].to_i
   end
 end
