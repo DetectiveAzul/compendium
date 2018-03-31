b0facede7005cae65f418d70f72b82e5a4f4e674
Jaime Lopez <detec.azul@gmail.com>
2018-03-31 11:48:50 +0100

    Added sql file with the creation of the tables

---
+++ db/dnd_compendium.sql
@@ -0,0 +1,39 @@
+DROP TABLE IF EXISTS learnings;
+DROP TABLE IF EXISTS spells;
+DROP TABLE IF EXISTS spellbooks;
+DROP TABLE IF EXISTS spell_slots;
+DROP TABLE IF EXISTS characters;
+
+CREATE TABLE characters(
+  id SERIAL8 PRIMARY KEY,
+  name VARCHAR(255),
+  level INT,
+  class VARCHAR(255),
+  writing_skill INT
+);
+CREATE TABLE spell_slots(
+  id SERIAL8 PRIMARY KEY,
+  char_id INT8 REFERENCES characters(id),
+  level INT,
+  quantity INT
+);
+CREATE TABLE spellbooks(
+  id SERIAL8 PRIMARY KEY,
+  name VARCHAR(255),
+  n_pages INT,
+  character_id INT8 REFERENCES characters(id)
+);
+CREATE TABLE spells(
+  id SERIAL8 PRIMARY KEY,
+  name VARCHAR(255),
+  class VARCHAR(255),
+  school VARCHAR(255),
+  level INT,
+  pages INT,
+  description TEXT
+);
+CREATE TABLE learnings(
+  id SERIAL8 PRIMARY KEY,
+  spell_id INT8 REFERENCES spells(id),
+  spellbook_id INT8 REFERENCES spellbooks(id)
+);
