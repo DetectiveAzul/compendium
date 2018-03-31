f0c077031949edb699a7ab5009ca85b2797fd49a
Jaime Lopez <detec.azul@gmail.com>
2018-03-31 11:29:20 +0100

    Created SqlRunner for sql queries

---
+++ db/sql_runner.rb
@@ -0,0 +1,14 @@
+require('pg')
+
+class SqlRunner
+  def self.run(sql, values)
+    begin
+      db = PG.connect({ dbname: 'dnd_compendium', host: 'localhost' })
+      db.prepare("query", sql)
+      result = db.exec_prepared("query", values)
+    ensure
+      db.close() unless db == nil
+    end
+    return result
+  end
+end
