acd387eb539829cd723b4790d5a9d3c8daa9f07e
Jaime Lopez <detec.azul@gmail.com>
2018-03-31 14:29:37 +0100

    Added basic CRUD on SpellSlot class

--- models/spell_slot.rb
+++ models/spell_slot.rb
@@ -7,4 +7,35 @@ class SpellSlot
     @level = options['id'].to_i
     @quantity = options['quantity'].to_i
   end
+
+  def self.all()
+    sql = "SELECT * FROM spell_slots"
+    spell_slots = SqlRunner.run(sql)
+    return spell_slots.map { |spell_slot| SpellSlot.new(spell_slot) }
+  end
+
+  def self.delete_all()
+    sql = "DELETE FROM spell_slots"
+    SqlRunner.run(sql)
+  end
+
+  def self.find(id)
+    sql = "SELECT * FROM spell_slots
+    WHERE id = $1"
+    values = [id]
+    spell_slot = SqlRunner.run(sql, values)
+    return SpellSlot.new(spell_slot.first) unless spell_slot.first == nil
+  end
+
+  def save()
+    sql = "INSERT INTO spell_slots
+    ( character_id, level, quantity )
+    VALUES
+    ( $1, $2, $3 )
+    RETURNING id"
+    values = [@character_id, @level, @quantity]
+    result = SqlRunner.run(sql, values)
+    @id = result.first['id'].to_i
+  end
+
 end
