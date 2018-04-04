ad65064bfd5ac583754f59c897f5ce6457c4bfce
Jaime Lopez <detec.azul@gmail.com>
2018-04-04 09:37:30 +0100

    hygienizing code and files

--- controllers/learning_controller.rb
+++ controllers/learning_controller.rb
@@ -2,61 +2,29 @@ require_relative('../models/learning')
 require_relative('../models/spellbook')
 require_relative('../models/spell')
 
-#index
-get '/learnings' do
-  @learnings = Learning.all()
-  erb(:"/learnings/index")
-end
 
-#new
-get '/learnings/new' do
-  @spellbooks = Spellbook.all()
-  @spells = Spell.all()
-  erb(:"/learnings/new")
-end
-
-#create
+#create from spells
 post '/learnings/spells' do
   learning = Learning.new(params)
   learning.save()
   redirect "/spells/#{learning.spell_id}"
 end
 
+#create from spellbooks
 post '/learnings/spellbooks' do
   learning = Learning.new(params)
   learning.save()
   redirect "/spellbooks/#{learning.spellbook_id}"
 end
 
-#show
-get '/learnings/:id' do
-  @learning = Learning.find(params['id'].to_i)
-  erb(:"/learnings/show")
-end
-
-#edit
-get '/learnings/:id/edit' do
-  @learning = Learning.find(params['id'].to_i)
-  @spellbooks = Spellbook.all()
-  @spells = Spell.all()
-  erb(:"/learnings/edit")
-end
-
-#update
-post '/learnings/:id' do
-  learning = Learning.new(params)
-  learning.update()
-  redirect "/learnings/#{params['id']}"
-
-end
-
-#delete
+#delete from spells
 post '/learnings/:id/delete/spells' do
   learning = Learning.find(params[:id].to_i)
   learning.delete()
   redirect "/spells/#{learning.spell_id}"
 end
 
+#delete from spellbooks
 post '/learnings/:id/delete/spellbooks' do
   learning = Learning.find(params[:id].to_i)
   learning.delete()
